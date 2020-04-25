b84f0a1ceaeb9b488716c2e246ff489f25f54e0a
salmanalfaridzi11 <salmanalfaridzi1112@gmail.com>
2020-04-25 14:57:11 +0700

    first commit

---
+++ UTS Praktek 2/UTS Praktek 2/RetailItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS_Praktek_2
+{
+    class RetailItem
+    {
+        private String description;
+        private int unitsOnHand;
+        private double price;
+
+        public RetailItem(String x, int y, double z)
+        {
+            description = x;
+            unitsOnHand = y;
+            price = z;
+        }
+
+        public String getDescription()
+        {
+            return description;
+        }
+
+        public void setDescription(String x)
+        {
+            description = x;
+        }
+
+        public int getUnitsOnHand()
+        {
+            return unitsOnHand;
+        }
+
+        public void setUnitsOnHand(int y)
+        {
+            unitsOnHand = y;
+        }
+
+        public double getPrice()
+        {
+            return price;
+        }
+
+        public void setPrice(double z)
+        {
+            price = z;
+        }
+    }
+}
